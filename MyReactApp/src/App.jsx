289c48036d412bd3580c706703f6b825972fb081
asapsonter <samuel.aarga@gmail.com>
2024-06-08 03:16:58 +0800

    add list

--- MyReactApp/src/App.jsx
+++ MyReactApp/src/App.jsx
@@ -5,12 +5,27 @@ import List from './components/List.jsx';
 
 function App() {
 
+  const frisbeeTeams = [
+    { id: 1, name: "N one", rank: 1 },
+    { id: 2, name: "nova", rank: 2 },
+    { id: 3, name: "zen", rank: 3 },
+    { id: 4, name: "storm", rank: 4 },
+    { id: 5, name: "power new", rank: 5 },
+  ];
 
+  const footballTeams = [
+    { id: 1, name: "N one", rank: 1 },
+    { id: 2, name: "nova", rank: 2 },
+    { id: 3, name: "zen", rank: 3 },
+    { id: 4, name: "storm", rank: 4 },
+    { id: 5, name: "power new", rank: 5 },
+  ];
 
   return (
     <>
       <div className="card">
-        <List/>
+        <List items={frisbeeTeams} category= "fruits"/>
+        <List items={footballTeams} category= "FootballTeams"/>
       </div>
     </>
   );
--- MyReactApp/src/components/List.jsx
+++ MyReactApp/src/components/List.jsx
@@ -1,21 +1,20 @@
 
 
 
-function List() {
-    const frisbeeTeams = [
-      { id: 1, name: "N one", rank: 1 },
-      { id: 2, name: "nova", rank: 2 },
-      { id: 3, name: "zen", rank: 3 },
-      { id: 4, name: "storm", rank: 4 },
-      { id: 5, name: "power new", rank: 5 },
-    ];
+function List(props) {
+ /// prop key
+  const teamsList = props.items;
+  const category = props.category;
     
     // eslint-disable-next-line react/jsx-key
-    const teams = frisbeeTeams.map(team => <li key={team.id}>
-                                                    {team.name}:
-                                                    &nbsp;{team.rank}</li>);
+    const teams = teamsList.map(team => 
+                    <li key={team.id}>
+                    {team.name}:
+              &nbsp;{team.rank}</li>);
 
-    return( <ol>{teams}</ol>)
+    return(<>
+    <h3>{category}</h3><ol>{teams}</ol>
+        </>)
 }
 
 
