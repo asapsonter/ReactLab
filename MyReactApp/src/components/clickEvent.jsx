c2579d2cc7e9ac13a5b6f1e60bf5ff7193aa2063
Sam Sonter <62767142+asapsonter@users.noreply.github.com>
2024-06-08 05:48:20 +0800

    end event click

--- MyReactApp/src/components/clickEvent.jsx
+++ MyReactApp/src/components/clickEvent.jsx
@@ -1,9 +1,23 @@
 
 
 function ClickEVent() {
-    const handleClick = () => console.log("gong si");
-    const handleClick2 = (name) => console.log(`${name}"gong si"`);
-    return(<button onClick={()=>handleClick2("bro")}>Click me</button>)
+    //const handleClick = () => console.log("gong si");
+    // const handleClick2 = (name) => console.log(`${name}"gong si"`);
+    // return(<button onClick={()=>handleClick2("bro")}>Click me</button>)
+    // let count = 0;
+    // const handleclick = (name) =>{
+    //     if(count < 3){
+    //      count ++  
+    //      console.log(`${name} "you clicked me" ${count} times`); 
+    //     } else {
+    //         console.log(`${name} stop clicking me`)
+    //     }
+        
+
+    // };
+    const handleclick = (event) => event.target.textContent = "So cool";
+
+    return <button onClick={(event) => handleclick(event)}>Click me</button>;
 
 }
 
